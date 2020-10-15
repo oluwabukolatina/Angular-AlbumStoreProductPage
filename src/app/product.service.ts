15aff47edfdcf234b9b085db9dbcb5d6f12033b7
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-10-15 11:44:17 +0100

    interface for data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http'
 import 'rxjs/add/operator/map'
+import {Album} from './album';
+import {Observable} from 'rxjs/Observable';
+
 
 @Injectable()
 export class ProductService {
