aacc537b4dbe4ba0a3f3e69cbace44a46c2865da
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-10-15 12:29:48 +0100

    displaying a list of product

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,17 +3,25 @@ import {Http, Response} from '@angular/http'
 import 'rxjs/add/operator/map'
 import {Album} from './album';
 import {Observable} from 'rxjs/Observable';
+import {Product} from './product';
 
 
 @Injectable()
 export class ProductService {
-  // private _productsUrl = '../assets/products.json'
+  // create a private class property that’s equal to the st
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json'
 
-
+  // his is a good practice for injecting an instance of a service into a class.
   constructor(private _http: Http) { }
+
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json())
+  }
+
 
 }
